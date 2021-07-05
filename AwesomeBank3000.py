30df4adac4745af92c67cb80103805d016cf79f6
Carina Marie Rose <86648826+carinasworld@users.noreply.github.com>
2021-07-05 15:06:02 +0200

    Update AwesomeBank3000.py

--- AwesomeBank3000.py
+++ AwesomeBank3000.py
@@ -11,3 +11,7 @@ taxes = [55800, 317100, 182000, 451800, 171450, 71400]
 
 for i in range(0, 6):
     print(f'Employee:{customers[i]}, salary:{salary[i]}, tax:{taxes[i]}')
+
+for i in range(0, 6):    
+    taxrate = taxes[i]/salary[i] * 100
+    print(f'Employee:{customers[i]}, {taxrate}')
